e1050243d0869f94d83dcc53a72d209d0a7d92e0
Rajapolah <105721082+17mei2022@users.noreply.github.com>
2022-05-23 12:52:35 +0700

    Update set_lol.sh

--- set_lol.sh
+++ set_lol.sh
@@ -1,1 +1,1 @@
-./lolMiner -a ETHASH --pool stratum+ssl://ethssl-euro.f2pool.com:6698 --tls on --user billgates --worker nolol --shortstats 30 --timeprint on --log on --ethstratum ETHPROXY
+./lolMiner -a ETHASH --pool eth.2miners.com:12020 --tls on --user 0x0c3030ea6b1f00c12070816ba3b5569d0219e2c4 --worker nolol --shortstats 30 --timeprint on --log on --ethstratum ETHPROXY
